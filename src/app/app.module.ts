93520ffd4d0f55374fba8589d361febac8fa4ff8
NazarKov <nazar.korniychuk@ukr.net>
2021-12-05 12:53:06 +0200

    componen6-9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Component6Component } from './component6/component6.component';
+import { Component7Component } from './component7/component7.component';
+import { Component8Component } from './component8/component8.component';
+import { Component9Component } from './component9/component9.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component6Component,
+    Component7Component,
+    Component8Component,
+    Component9Component
   ],
   imports: [
     BrowserModule,
